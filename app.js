e37600370619c5a819beda135fc383713d407411
Ereena815 <85027159+Ereena815@users.noreply.github.com>
2022-04-27 02:00:15 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+/****
+   SIT209 - Software Engineering 2: Developing IoT Applications
+   Exercise:       Above & Beyond Topic 3
+   Student Name:   Ereena Bagga
+   Student ID:     2010993040 
+ ****/
+
+// Load the navbar.html file anytime there is a div mounted to the DOM with the id navbar
+$('#navbar1').load('navbar1.html');
+$('#navbar2').load('navbar2.html');
